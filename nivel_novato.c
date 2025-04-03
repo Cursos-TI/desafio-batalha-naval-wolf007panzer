733808f7b6ac59c602dd7610e5d13bfe4b3e52ed
wolf007panzer <randleysantos1@gmail.com>
2025-04-03 22:08:27 +0000

    criei o nivel_novato

---
+++ nivel_novato.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+// codigo do tabuleiro onde o mar e 0
+
+void tabuleiro (int tabuleiro[10][10]){
+
+ char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+ printf("TABULEIRO DE BATALHA NAVAL\n");
+ printf(" ");
+
+ for(int j = 0; j < 10; j++)
+ {
+ printf("%c ", linha[j]);
+ }
+ printf("\n");
+
+ for(int i = 0; i < 10; i++)
+ {
+ printf("%d ", i+1);
+ for(int j = 0; j < 10; j++)
+ {
+  printf("%d ", tabuleiro[i][j]);
+ }
+ printf("\n");
+ }
+}
+
+int main () {
+
+//posição onde os navios estão representados pelo numero 3
+
+int navio[10][10] = {0};
+
+navio [1][1] = 3;
+navio [1][2] = 3;
+navio [1][3] = 3;
+
+navio [1][9] = 3;
+navio [2][9] = 3;
+navio [3][9] = 3;
+
+navio [5][5] = 3;
+navio [6][5] = 3;
+navio [7][5] = 3;
+
+navio[8][2] = 3;
+navio[8][3] = 3;
+navio[8][4] = 3;
+
+tabuleiro(navio);
+
+return 0;
+
+}
