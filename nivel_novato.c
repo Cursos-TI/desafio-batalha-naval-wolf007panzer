4b6d98fa8f9d7e89135b0eb13d9e59ccc3e3ae08
wolf007panzer <randleysantos1@gmail.com>
2025-04-03 23:25:30 +0000

    alterei o nivel_novato

--- nivel_novato.c
+++ nivel_novato.c
@@ -16,7 +16,7 @@ void tabuleiro (int tabuleiro[10][10]){
 
  for(int i = 0; i < 10; i++)
  {
- printf("%d ", i+1);
+ printf("%2d ", i+1);
  for(int j = 0; j < 10; j++)
  {
   printf("%d ", tabuleiro[i][j]);
