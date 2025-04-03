cfdc8e2efe42ebc89cfd9bf9eeda6ba4dbab0272
wolf007panzer <randleysantos1@gmail.com>
2025-04-03 22:57:45 +0000

    criei o nivel_aventureiro

---
+++ nivel_aventureiro.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+// codigo do tabuleiro onde o mar e 0
+
+void tabuleiro (int tabuleiro[10][10]){
+
+ char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+ printf("TABULEIRO DE BATALHA NAVAL\n");
+ printf(" ");
+
+ for(int j = 0; j < 10; j++)
+ {
+ printf("%c ", linha[j]);
+ }
+ printf("\n");
+
+ for(int i = 0; i < 10; i++)
+ {
+ printf("%2d ", i+1);
+ for(int j = 0; j < 10; j++)
+ {
+  printf("%d ", tabuleiro[i][j]);
+ }
+ printf("\n");
+ }
+}
+
+int main () {
+
+printf("Nivel aventureiro\n");
+printf("Commit");
+
+//posição onde os navios estão representados pelo numero 3
+
+int navio[10][10] = {0};
+
+navio [1][4] = 3;
+navio [1][5] = 3;
+navio [1][6] = 3;
+
+navio [1][9] = 3;
+navio [2][9] = 3;
+navio [3][9] = 3;
+
+for (int i = 0; i < 3; i++)
+    {
+      navio[i][i] = 3;  
+      for (int i = 4; i < 7; i++)
+      {
+        navio[i][i] = 3;
+      }
+    }
+      
+tabuleiro(navio);
+
+return 0;
+
+}
